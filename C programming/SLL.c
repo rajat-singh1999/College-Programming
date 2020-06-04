79e2fe5c6e63e9536017bdfc539fac769f9bb58d
Rajat Singh <rsingh1734@gmail.com>
2020-06-04 08:39:13 +0530

    Update SLL.c

--- C programming/SLL.c
+++ C programming/SLL.c
@@ -19,10 +19,19 @@ void insertFirst()
 	int i;
 	newnode = (struct node*)malloc(sizeof(struct node));
 	printf("Enter data: ");
-	i = scanf("%d", &i);
-	newnode->data = i;
-	newnode->next = head;
-	head = newnode;
+	scanf("%d", &i);
+	if(head == NULL)
+	{	
+		newnode->data = i;
+		newnode->next = NULL;
+		head = newnode;
+	}
+	else
+	{
+		newnode->data = i;
+		newnode->next = head;
+		head = newnode;
+	}
 }
 
 void display()
@@ -43,10 +52,68 @@ void display()
 	}
 }
 
+void displayReserve()
+{
+	if(head == NULL)
+		printf("\nNo node to display\n");
+	else
+	{
+		struct node *p = head;
+		int arr[100];
+		int i = -1;
+		while(p != NULL)
+		{	
+			i++;
+			arr[i]	= p->data;
+			p = p->next;
+		}
+		printf("\n");
+		while(i >= 0)
+		{
+			printf("<-%d", arr[i]);
+			--i;
+		}
+	}
+}
+
+void reverseList()
+{
+    struct node *prevNode, *curNode;
+
+    if(head != NULL)
+    {
+        prevNode = head;
+        curNode = head->next;
+        head = head->next;
+
+        prevNode->next = NULL; // Make first node as last node
+
+        while(head != NULL)
+        {
+            head = head->next;
+            curNode->next = prevNode;
+
+            prevNode = curNode;
+            curNode = head;
+        }
+
+        head = prevNode; // Make last node as head
+
+        printf("SUCCESSFULLY REVERSED LIST\n");
+    }
+}
+
+
 int main()
 {
 	initialize();
 	insertFirst();
+	insertFirst();
+	insertFirst();
+	insertFirst();
+	display();
+	displayReserve();
+	reverseList();
 	display();
 	return 0;
 }
